ad6696ad37e2683244cae4512e2afa48d384668d
rohit-singh-hitman <66105560+rohit-singh-hitman@users.noreply.github.com>
2021-04-22 11:02:08 +0530

    insta

--- src/Insta.js
+++ src/Insta.js
@@ -1,65 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Insta = () => {
-  return (
-    <>
-      <div
-        id="carouselExampleIndicators"
-        class="carousel slide"
-        data-ride="carousel"
-      >
-        <ol class="carousel-indicators">
-          <li
-            data-target="#carouselExampleIndicators"
-            data-slide-to="0"
-            class="active"
-          ></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img
-              class=""
-              src="https://getwallpapers.com/wallpaper/full/7/1/3/1017956-amazing-friends-tv-show-wallpapers-1920x1080.jpg"
-              alt="First slide"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              class=""
-              src="https://media.glamour.com/photos/5696373593ef4b09520fc68d/master/w_1280,c_limit/entertainment-2013-04-friends-cast-1-main.jpg"
-              alt="Second slide"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              class=""
-              src="https://media.gettyimages.com/photos/matt-leblanc-who-plays-joey-on-the-hit-nbc-series-friends-makes-a-picture-id2575772?k=6&m=2575772&s=612x612&w=0&h=as1GiRVKPvxSaAnycDeZtr7Uo0ThYSjc_FHadRXFuXo="
-              alt="Third slide"
-            />
-          </div>
-        </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </>
-  );
-};
+import Carousel from "react-elastic-carousel";
+import "./styles.css";
+
+class Insta extends React.Component {
+  constructor(props) {
+    super(props);
+    this.breakPoints = [
+      { width: 1, itemsToShow: 1 },
+      { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
+      { width: 850, itemsToShow: 4 },
+      { width: 1150, itemsToShow: 5, itemsToScroll: 2 },
+      { width: 1450, itemsToShow: 6 },
+      { width: 1750, itemsToShow: 8 }
+    ];
+  }
+  render() {
+    return (
+      <Carousel breakPoints={this.breakPoints}>
+        <img
+          src="https://th.bing.com/th/id/OIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK?w=329&h=185&c=7&o=5&dpr=1.25&pid=1.7"
+          alt=""
+          style={{ width: "200px", height: "200px" }}
+        />
+        <img
+          src="https://th.bing.com/th/id/OIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK?w=329&h=185&c=7&o=5&dpr=1.25&pid=1.7"
+          alt=""
+          style={{ width: "200px", height: "200px" }}
+        />
+        <img
+          src="https://th.bing.com/th/id/OIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK?w=329&h=185&c=7&o=5&dpr=1.25&pid=1.7"
+          alt=""
+          style={{ width: "200px", height: "200px" }}
+        />
+        <img
+          src="https://th.bing.com/th/id/OIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK?w=329&h=185&c=7&o=5&dpr=1.25&pid=1.7"
+          alt=""
+          style={{ width: "200px", height: "200px" }}
+        />
+        <img
+          src="https://th.bing.com/th/id/OIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK?w=329&h=185&c=7&o=5&dpr=1.25&pid=1.7"
+          alt=""
+          style={{ width: "200px", height: "200px" }}
+        />
+        <img
+          src="https://th.bing.com/th/id/OIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK?w=329&h=185&c=7&o=5&dpr=1.25&pid=1.7"
+          alt=""
+          style={{ width: "200px", height: "200px" }}
+        />
+        <img
+          src="https://th.bing.com/th/id/OIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK?w=329&h=185&c=7&o=5&dpr=1.25&pid=1.7"
+          alt=""
+          style={{ width: "200px", height: "200px" }}
+        />
+        <img
+          src="https://th.bing.com/th/id/OIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK?w=329&h=185&c=7&o=5&dpr=1.25&pid=1.7"
+          alt=""
+          style={{ width: "200px", height: "200px" }}
+        />
+      </Carousel>
+    );
+  }
+}
+
 export default Insta;
